bd12758d40d2196d87b9a5bda96353dcd4194d6b
mjsoftking <1406993111@qq.com>
2021-10-28 15:47:01 +0800

    提交

--- app/src/main/java/com/osard/alipaylibraryapp/MainActivity.java
+++ app/src/main/java/com/osard/alipaylibraryapp/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.osard.alipaylibrary.callback.AliUserCancel;
+import com.osard.alipaylibrary.callback.auth.AliAuthFail;
+import com.osard.alipaylibrary.callback.auth.AliAuthSucceed;
 import com.osard.alipaylibrary.callback.pay.AliPayFail;
 import com.osard.alipaylibrary.callback.pay.AliPaySucceed;
+import com.osard.alipaylibrary.result.AliAuthResult;
 import com.osard.alipaylibrary.result.AliPayResult;
+import com.osard.alipaylibrary.util.AliAuthUtil;
 import com.osard.alipaylibrary.util.AliPayUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,28 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AliPayUtil.newInstance()
-                .setSucceed(new AliPaySucceed() {
-                    @Override
-                    public void succeed(AliPayResult aliPayResult) {
+                .setSucceed(aliPayResult -> {
 
-                    }
                 })
-                .setFail(new AliPayFail() {
-                    @Override
-                    public void fail(AliPayResult aliPayResult) {
-                        //支付宝支付返回的状态码对应状态
-                        String resultStatus = aliPayResult.getResultStatus();
-                        //支付宝公共错误码
-                        String resultCode = aliPayResult.getResultCode();
-                    }
+                .setFail(aliPayResult -> {
+                    //支付宝支付返回的状态码对应状态
+                    String resultStatus = aliPayResult.getResultStatus();
+                    //支付宝公共错误码
+                    String resultCode = aliPayResult.getResultCode();
                 })
-                .setUserCancel(new AliUserCancel() {
-                    @Override
-                    public void userCancel() {
+                .setUserCancel(() -> {
 
-                    }
                 })
                 .payV2(this, "从服务器获取最终签名后的支付信息");
+
+
+        AliAuthUtil.newInstance()
+                .setSucceed(aliAuthResult -> {
+
+                })
+                .setUserCancel(() -> {
+
+                })
+                .setFail(aliAuthResult -> {
+                    //支付宝登录认证的ResultStatus
+                    String resultStatus = aliAuthResult.getResultStatus();
+                    //支付宝登录认证的ResultCode
+                    String resultCode = aliAuthResult.getResultCode();
+                })
+                .authV2(this, "从服务器获取最终签名后的登录信息");
 
     }
 }
